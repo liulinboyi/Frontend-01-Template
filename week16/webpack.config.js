611985936a3450f684162779a7a736d2bb59591e
liulinboyi <814921718@qq.com>
2020-07-31 13:06:23 +0800

    sixteenth week Homework & summary

---
+++ week16/webpack.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  entry: "./main.js",
+  module: {
+    rules: [{
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                {
+                  pragma: "createElement"
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.view/,
+        use: {
+          loader: require.resolve("./myloader.js")
+        }
+      }
+    ],
+  },
+  mode: "development",
+  optimization: {
+    minimize: false,
+  },
+  devServer: {
+    // contentBase: path.join(__dirname, "dist"),
+    // compress:true,
+    hot: true,
+    port: "8000",
+    inline: true,
+    open: true,
+    overlay: true,
+    proxy: {
+      "/api": {
+        target: "",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+      },
+    },
+  },
+};
